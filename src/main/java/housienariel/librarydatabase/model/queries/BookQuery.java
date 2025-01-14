3c30aa52a18ff903011a762d28ba68a638d91e55
Ari0115 <arihumvarper@gmail.com>
2025-01-14 13:17:13 +0100

    .

--- src/main/java/housienariel/librarydatabase/model/queries/BookQuery.java
+++ src/main/java/housienariel/librarydatabase/model/queries/BookQuery.java
@@ -170,32 +170,18 @@ public class BookQuery implements BookDAO {
     }
 
     @Override
-    public List<Author> getBookAuthors(String isbn) throws BooksDbException {
+    public List<Author> searchAuthorsByName(String name) throws BooksDbException {
+        List<Author> authors = new ArrayList<>();
         try {
-            Document query = new Document("ISBN", isbn);
-            Document book = bookCollection.find(query).first();
-
-            if (book != null && book.containsKey("author_id")) {
-                List<Integer> authorIds = book.getList("author_id", Integer.class);
-
-                List<Author> authors = new ArrayList<>();
-                for (Integer authorId : authorIds) {
-                    Document authorQuery = new Document("_id", authorId);
-                    Document authorDoc = bookCollection.find(authorQuery).first();
-
-                    if (authorDoc != null) {
-                        String name = authorDoc.getString("name");
-                        authors.add(new Author(name));
-                    }
-                }
-
-                return authors;
+            Document query = new Document("name", new Document("$regex", name).append("$options", "i"));
+            for (Document doc : bookCollection.find(query)) {
+                String authorName = doc.getString("name");
+                authors.add(new Author(authorName));
             }
-
-            return new ArrayList<>();
         } catch (Exception e) {
-            throw new BooksDbException("Error getting book authors: " + e.getMessage(), e);
+            throw new BooksDbException("Error searching authors by name: " + e.getMessage(), e);
         }
+        return authors;
     }
 
 }
